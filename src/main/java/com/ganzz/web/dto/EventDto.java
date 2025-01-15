c13e7feb7997ebf2a19f57b2d72bc559b32902c6
Ganziuc Alexandr <ganziucalexandr@gmail.com>
2025-01-15 15:21:04 +0200

    Added validation of input data for events

--- src/main/java/com/ganzz/web/dto/EventDto.java
+++ src/main/java/com/ganzz/web/dto/EventDto.java
@@ -2,6 +2,10 @@ package com.ganzz.web.dto;
 
 
 import com.ganzz.web.models.Section;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,11 +20,17 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class EventDto {
     private Long id;
+    @NotBlank(message = "Event name should not be empty")
     private String name;
+    @NotNull(message = "Start time cannot be null")
+    @FutureOrPresent(message = "Start time must be in the present or future")
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime startTime;
+    @NotNull(message = "End time cannot be null")
+    @Future(message = "End time must be in the future")
     @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime endTime;
+    @NotBlank(message = "Event type should not be empty")
     private String type;
     private String photoUrl;
     private LocalDateTime createdOn;
